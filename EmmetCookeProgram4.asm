cf9136ae9da07330c9ab008b52b70d2e8059e65e
Emmet <emmetcooke@gmail.com>
2017-10-30 10:55:14 -0500

    Add project files.

---
+++ EmmetCookeProgram4.asm
@@ -0,0 +1,25 @@
+TITLE Program Template     (EmmetCookeProgram4.asm)
+
+; Author: Emmet Cooke
+; Course / Project ID: CS271-400 Program 4    Date: 10/30/2017
+; Description:
+
+INCLUDE Irvine32.inc
+
+; (insert constant definitions here)
+
+.data
+
+; (insert variable definitions here)
+
+.code
+main PROC
+
+; (insert executable instructions here)
+
+	exit	; exit to operating system
+main ENDP
+
+; (insert additional procedures here)
+
+END main
