c18de20cb8616dbd76a1b6f33176dde06e000b6e
Emmet <emmetcooke@gmail.com>
2017-10-30 19:39:23 -0500

    Completed the writeComposites procedure and expanded upon the comments

--- EmmetCookeProgram4.asm
+++ EmmetCookeProgram4.asm
@@ -1,8 +1,12 @@
-TITLE Program Template     (EmmetCookeProgram4.asm)
+TITLE Composite Numbers    (EmmetCookeProgram4.asm)
 
 ; Author: Emmet Cooke
 ; Course / Project ID: CS271-400 Program 4    Date: 10/30/2017
-; Description:
+; Description: This program asks the user for the number of composite numbers they
+; would like to see printed. It takes this number and then goes through as many numbers
+; as it needs to until that number of composites are printed. It checks if a number is
+; prime, and if it is not, it prints it.
+; EC1: The output numbers are aligned.
 
 INCLUDE Irvine32.inc
 
@@ -14,6 +18,7 @@ TAB = 9
 	; Strings for the introduction
 	programTitle	BYTE	"Program   : Composite Numbers",0
 	programmerName	BYTE	"Programmer: Emmet Cooke",0
+	extraCredit1	BYTE	"EC1: Numbers are aligned in output",0
 
 	; Prompt to get data
 	prompt1			BYTE	"Please enter the number of composite numbers you "
@@ -25,7 +30,11 @@ TAB = 9
 
 	; Variables to hold data
 	userInput		DWORD	?
+	prime			DWORD	0
+	numbersPrinted	DWORD	0
 
+	; Strings for farewell
+	goodbye			BYTE	"Thank you for using this program. Goodbye.",0
 .code
 main PROC
 
@@ -38,8 +47,10 @@ main PROC
 
 ; Show Composites
 	call	writeComposites
+	call	Crlf
 
 ; Farewell
+	call	farewellMessage
 
 	exit	; exit to operating system
 main ENDP
@@ -58,8 +69,25 @@ introduceProgrammer PROC USES edx
 	mov		edx, OFFSET programmerName
 	call	WriteString
 	call	Crlf
+	mov		edx, OFFSET extraCredit1
+	call	WriteString
+	call	Crlf
 	ret
 introduceProgrammer ENDP
+
+;-------------------------------------
+; Procedure to output goodbye message
+; recieves: none
+; returns: none
+; preconditions: none
+; registers changed: edx
+;-------------------------------------
+farewellMessage PROC USES edx
+	mov		edx, OFFSET goodbye
+	call	WriteString
+	call	Crlf
+	ret
+farewellMessage ENDP
 
 ;-------------------------------------
 ; Procedure to get an integer from the user
@@ -76,20 +104,20 @@ loopStart:
 	mov		edx, OFFSET prompt2
 	call	WriteString
 	call	ReadInt
-	push	eax
+	push	eax				; Send eax to validateInput
 	call	Crlf
 	call	validateInput
 	pop		eax
-	cmp		bx, 1
-	je		invalidInput
-	mov		ecx, eax
+	cmp		bx, 1			; if bx equal to 1 as set by validateInput
+	je		invalidInput	; the input is not valid
+	mov		userInput, eax	; pass the valid entry to a global variable
 	jmp		endGetUserData
 
 invalidInput:
 	mov		edx, OFFSET error
 	call	WriteString
 	call	Crlf
-	jmp		loopStart
+	jmp		loopStart		; try again
 
 endGetUserData:	
 	ret
@@ -98,67 +126,130 @@ getUserData ENDP
 ;-------------------------------------
 ; Procedure to validate the user input
 ; recieves: user input value
-; returns: none
+; returns: bx
 ; preconditions: none
-; registers changed: 
+; registers changed: eax ebp esp
 ;-------------------------------------
 validateInput PROC
 	push	ebp
 	mov		ebp, esp
 	mov		eax, [ebp + 8]
-	cmp		eax, 1
+	; Check if the number is below 1
+	cmp		eax, 1			
 	jb		badInput
+	; Check if the number is above 400
 	cmp		eax, UPPER_LIMIT
 	ja		badInput
 	pop		ebp
-	mov		bx, 0
+	mov		bx, 0	; The input is good
 	ret		
 
 badInput:
 	pop		ebp
-	mov		bx, 1
+	mov		bx, 1	; The input is bad
 	ret		
 validateInput ENDP
 
 ;-------------------------------------
-; Procedure to validate the user input
-; recieves: user input value
+; Procedure to print out the composite numbers
+; recieves: none
 ; returns: none
 ; preconditions: none
-; registers changed: 
+; registers changed: eax, ebx, ecx, edx
 ;-------------------------------------
 writeComposites PROC USES eax ebx
-	mov		ebx, 10
-	mov		eax, 1
+	mov			ecx, UPPER_LIMIT
+	mov			ebx, 10
+	mov			eax, 4
+
 printStart:
-	call	WriteDec
-	dec		ebx
-	cmp		ebx, 0
-	je		rowBreak
+	push		eax
+	call		isPrime
+	pop			eax
+	; Check if the number is prime
+	cmp			prime, 1
+	jne			primeNumber	; the number is prime
+	call		WriteDec
+	inc			numbersPrinted	; tracks how many numbers are printed
+	call		WriteTab
+	; Check if at the end of a row
+	dec			ebx
+	cmp			ebx, 0	
+	je			rowBreak
+continuePrint:
+	push		eax
+	; Check how many numbers have been printed
+	mov			eax, userInput
+	cmp			eax, numbersPrinted
+	je			endPrint
+	pop			eax
+	inc			eax
+	loop		printStart
+
+; If the number is prime, skip the printing process
+primeNumber:
+	inc			eax
+	loop		printStart
+
+; If 10 have been printed, create a new row
+rowBreak:
+	mov			ebx, 10
+	call		Crlf
+	jmp			continuePrint
+
+endPrint:
+	pop			eax
+	ret
+writeComposites ENDP
+
+;-------------------------------------
+; Procedure to check if a number is prime
+; recieves: eax
+; returns: prime
+; preconditions: none
+; registers changed: eax ebx ecx edx
+;-------------------------------------
+isPrime PROC USES eax ebx ecx edx
+	mov		ebx, 2
+; Determine how many numbers need to be checked
+	mov		ecx, eax
+	sub		ecx, ebx
+	dec		ecx	; So that the final number isn't checked
+
+checkPrime:
+	xor		edx, edx
+	push	ecx
+	mov		ecx, eax	; Holds eax during idiv
+	idiv	ebx
+	mov		eax, ecx
+	pop		ecx
+	cmp		edx, 0		; If no remainder, it's prime
+	je		notPrime
+	inc		ebx
+	loop	checkPrime
+
+	mov		prime, 0	; Not prime
+	ret
+
+notPrime:
+	mov		prime, 1
+	ret
+
+isPrime ENDP
+
+;-------------------------------------
+; Procedure to print a tab between numbers
+; recieves: none
+; returns: none
+; preconditions: none
+; registers changed: eax
+;-------------------------------------
+writeTab PROC
 	push	eax
 	mov		al, TAB
 	call	WriteChar
 	pop		eax
-writeNumber:
-	inc		eax
-	loop	printStart
 	ret
-
-rowBreak:
-	mov		ebx, 10
-	call	Crlf
-	jmp		WriteNumber
-writeComposites ENDP
-
-;-------------------------------------
-; Procedure to validate the user input
-; recieves: user input value
-; returns: none
-; preconditions: none
-; registers changed: 
-;-------------------------------------
-isPrime PROC
-	ret
-isPrime ENDP
+writeTab ENDP
 
 END main
